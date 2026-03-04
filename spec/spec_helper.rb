14c038ae7e3fe028240df9678abd4a8d9e043499
Micah Geisel <micah@botandrose.com>
2026-03-04 17:23:27 -0600

    add simplecov for test coverage tracking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start
+
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
