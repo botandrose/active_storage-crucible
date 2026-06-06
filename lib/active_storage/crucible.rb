1904f6b230c0a25505927188f509a43c53f89387
Micah Geisel <micah@botandrose.com>
2026-06-06 15:01:03 +0100

    async_variants is a direct dependency.

--- lib/active_storage/crucible.rb
+++ lib/active_storage/crucible.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_storage/async_variants"
+
 require_relative "crucible/version"
 require_relative "crucible/client"
 require_relative "crucible/presigned_url"
